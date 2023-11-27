37252338ab9325109ba3d9cb75e40013129d513a
MattanCohen <mc.3552883@gmail.com>
2023-11-27 13:20:12 +0200

    add build path edits to build buitton

--- Unity Scripts/Editor/Build Button Options/BuildButtonWindow.cs
+++ Unity Scripts/Editor/Build Button Options/BuildButtonWindow.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 public class BuildButtonWindow : EditorWindow
 {
     private static BuildTarget previousPlatform;
+    public static string AndroidBuildPath   => "Builds/" + EditorPrefs.GetString("AndroidBuildPath") + ".apk";
+    public static string PcBuildPath        => "Builds/" + EditorPrefs.GetString("PCBuildPath") + ".exe";
+
 
     [MenuItem("Build/Build for PC or Android")]
     private static void BuildForPCAndAndroidMenuItem()
@@ -43,7 +46,7 @@ public class BuildButtonWindow : EditorWindow
             BuildPipeline.BuildPlayer(pcBuildOptions);
             BuildPipeline.BuildPlayer(androidBuildOptions);
 
-            // Return to PC platform if previous platform was PC
+            // Return to PC platform if the previous platform was PC
             if (previousPlatform == BuildTarget.StandaloneWindows)
             {
                 SwitchToPCPlatform();
@@ -59,14 +62,13 @@ public class BuildButtonWindow : EditorWindow
             BuildPlayerOptions androidBuildOptions = CreateAndroidBuildOptions();
             BuildPipeline.BuildPlayer(androidBuildOptions);
 
-            // Return to PC platform if previous platform was PC
+            // Return to PC platform if the previous platform was PC
             if (previousPlatform == BuildTarget.StandaloneWindows)
             {
                 SwitchToPCPlatform();
             }
         }
     }
-
     public static void BuildForPC()
     {
         bool confirmed = EditorUtility.DisplayDialog("Confirmation", "Build for PC? (This might take a while...)", "Yes", "Not Now");
@@ -77,18 +79,23 @@ public class BuildButtonWindow : EditorWindow
         }
     }
 
+    public static void ShowEditBuildPaths()
+    {
+        EditBuildPathsWindow window = EditorWindow.GetWindow<EditBuildPathsWindow>("Edit Build Paths");
+        window.Show();
+    }
+
     private static BuildPlayerOptions CreatePCBuildOptions()
     {
         string gameName = PlayerSettings.productName;
         string version = PlayerSettings.bundleVersion;
-        string pcBuildPath = $"Builds/PC/{gameName} {version}/{gameName}.exe";
 
         BuildPlayerOptions pcBuildOptions = new BuildPlayerOptions();
         pcBuildOptions.scenes = EditorBuildSettingsScene.GetActiveSceneList(EditorBuildSettings.scenes);
-        pcBuildOptions.locationPathName = pcBuildPath;
+        pcBuildOptions.locationPathName = PcBuildPath;
         pcBuildOptions.target = BuildTarget.StandaloneWindows;
         pcBuildOptions.options = BuildOptions.None;
-        
+
         // Exclude Burst debug information folder from PC build
         pcBuildOptions.extraScriptingDefines = new[] { "ENABLE_BURST_DEBUG_INFORMATION=0" };
 
@@ -99,11 +106,10 @@ public class BuildButtonWindow : EditorWindow
     {
         string gameName = PlayerSettings.productName;
         string version = PlayerSettings.bundleVersion;
-        string androidBuildPath = $"Builds/Android/{gameName}.apk";
 
-         BuildPlayerOptions androidBuildOptions = new BuildPlayerOptions();
+        BuildPlayerOptions androidBuildOptions = new BuildPlayerOptions();
         androidBuildOptions.scenes = EditorBuildSettingsScene.GetActiveSceneList(EditorBuildSettings.scenes);
-        androidBuildOptions.locationPathName = androidBuildPath;
+        androidBuildOptions.locationPathName = AndroidBuildPath;
         androidBuildOptions.target = BuildTarget.Android;
         androidBuildOptions.options = BuildOptions.None;
 
@@ -126,6 +132,80 @@ public class BuildButtonWindow : EditorWindow
     }
 }
 
+public class EditBuildPathsWindow : EditorWindow
+{
+    private string androidBuildPath;
+    private string pcBuildPath;
+    bool showFullPaths = false;
+
+    static string fullPathPrefix;
+    private void OnEnable()
+    {
+        EditBuildPathsWindow window = (EditBuildPathsWindow)EditorWindow.GetWindow(typeof(EditBuildPathsWindow));
+        fullPathPrefix = Application.dataPath.Substring(0, Application.dataPath.LastIndexOf("/"));
+
+        // find paths without Builds/ before and with no file extensions
+        androidBuildPath = EditorPrefs.GetString("AndroidBuildPath");
+        pcBuildPath = EditorPrefs.GetString("PCBuildPath");
+    }
+
+    string AddPrefix(string path) => fullPathPrefix + "/Builds/" + path;
+    string RemovePrefix(string path) => path.Substring("/Builds/".Length + fullPathPrefix.Length);
+    private void OnGUI()
+    {
+        // show toggle to show full paths
+        showFullPaths = GUILayout.Toggle(showFullPaths, "Show Full Paths");
+
+        // set the min size of the window
+        minSize = new Vector2(showFullPaths ? 800 : 600, 150);    
+
+        GUILayout.Label("Edit Build Paths", EditorStyles.boldLabel);
+
+        // Input field for Android build path
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Android Build Path:", GUILayout.Width(150));
+        if (showFullPaths){
+            string androidPath = AddPrefix(androidBuildPath) + ".apk";
+            androidBuildPath = RemovePrefix(GUILayout.TextField(androidPath));
+            androidBuildPath = androidBuildPath.Substring(0, androidBuildPath.Length - ".apk".Length);
+            Debug.Log($"android {androidBuildPath}");
+        }
+        else{
+            androidBuildPath = GUILayout.TextField(androidBuildPath);
+        }
+        GUILayout.EndHorizontal();
+
+        // Input field for PC build path
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("PC Build Path:", GUILayout.Width(150));
+        if (showFullPaths){
+            string pcPath = AddPrefix(pcBuildPath) + ".exe";
+            pcBuildPath = RemovePrefix(GUILayout.TextField(pcPath));
+            pcBuildPath = pcBuildPath.Substring(0, pcBuildPath.Length - ".exe".Length);
+        }
+        else{
+            pcBuildPath = GUILayout.TextField(pcBuildPath);
+        }
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(20);
+
+        // Save button
+        if (GUILayout.Button("Save", GUILayout.Height(30)))
+        {
+            SaveBuildPaths();
+        }
+    }
+
+    private void SaveBuildPaths()
+    {
+        // Save the build paths to EditorPrefs
+        EditorPrefs.SetString("AndroidBuildPath", androidBuildPath);
+        EditorPrefs.SetString("PCBuildPath", pcBuildPath);
+        Close();
+    }
+
+}
 public class BuildOptionsWindow : EditorWindow
 {
     private const int buttonHeight = 40;
@@ -154,6 +234,14 @@ public class BuildOptionsWindow : EditorWindow
         if (GUILayout.Button("Build for PC", GUILayout.Height(buttonHeight)))
         {
             BuildButtonWindow.BuildForPC();
+            Close();
+        }
+
+        GUILayout.Space(buttonMargin);
+
+        if (GUILayout.Button("Edit Build Paths", GUILayout.Height(buttonHeight)))
+        {
+            BuildButtonWindow.ShowEditBuildPaths();
             Close();
         }
     }
